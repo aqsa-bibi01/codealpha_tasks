60cf10d848b1c68da8c84e5d86f55afdc58f8d80
aqsa-bibi01 <aqsa72264@gmail.com>
2026-07-01 10:22:22 +0500

    Models

---
+++ Post.js
@@ -0,0 +1,9 @@
+const mongoose=require('mongoose');
+const s=new mongoose.Schema({
+  user:{type:mongoose.Schema.Types.ObjectId,ref:'User',required:true},
+  content:{type:String,required:true},
+  image:{type:String,default:''},
+  likes:[{type:mongoose.Schema.Types.ObjectId,ref:'User'}],
+  comments:[{user:{type:mongoose.Schema.Types.ObjectId,ref:'User'},text:String,createdAt:{type:Date,default:Date.now}}]
+},{timestamps:true});
+module.exports=mongoose.model('Post',s);
