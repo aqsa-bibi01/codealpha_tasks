0159c89bc158586d07a5d0d1ee40f320baa5aafb
aqsa-bibi01 <aqsa72264@gmail.com>
2026-07-01 10:19:02 +0500

    Backend

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+app.use('/api/auth',     require('./routes/auth'));
+app.use('/api/products', require('./routes/products'));
+app.use('/api/orders',   require('./routes/orders'));
+app.use('/api/cart',     require('./routes/cart'));
+
+app.listen(process.env.PORT, () =>
+  console.log('Server running on port ' + process.env.PORT));
