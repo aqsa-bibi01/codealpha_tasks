cb98176655ba4c75896eea28fbb900ab579c0b43
aqsa-bibi01 <aqsa72264@gmail.com>
2026-07-01 10:25:24 +0500

    Backend

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express=require('express'),mongoose=require('mongoose'),cors=require('cors'),http=require('http');
+const{Server}=require('socket.io');
 require('dotenv').config();
+const app=express(),server=http.createServer(app);
+const io=new Server(server,{cors:{origin:'*'}});
+app.use(cors());app.use(express.json());
+mongoose.connect(process.env.MONGO_URI).then(()=>console.log('MongoDB connected')).catch(console.error);
+app.use('/api/auth',require('./routes/auth'));
+app.use('/api/projects',require('./routes/projects'));
+app.use('/api/tasks',require('./routes/tasks'));
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+io.on('connection',socket=>{
+  console.log('User connected:',socket.id);
+  socket.on('join-project',projectId=>socket.join(projectId));
+  socket.on('task-update',data=>io.to(data.projectId).emit('task-updated',data));
+  socket.on('new-comment',data=>io.to(data.projectId).emit('comment-added',data));
+  socket.on('disconnect',()=>console.log('User disconnected'));
+});
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
-
-app.use('/api/auth',     require('./routes/auth'));
-app.use('/api/products', require('./routes/products'));
-app.use('/api/orders',   require('./routes/orders'));
-app.use('/api/cart',     require('./routes/cart'));
-
-app.listen(process.env.PORT, () =>
-  console.log('Server running on port ' + process.env.PORT));
+server.listen(process.env.PORT,()=>console.log('PM API on port '+process.env.PORT));
+module.exports={io};
